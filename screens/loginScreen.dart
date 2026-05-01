6cd9945aa75108af1ecdfd4752fa219f6aba9e17
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-05-01 11:00:25 -0300

    SingleChildScrollView + ConstrainedBox para arrumar problema do usúario

--- screens/loginScreen.dart
+++ screens/loginScreen.dart
@@ -9,148 +9,168 @@ class Loginscreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Inicializa as dimensões baseadas no contexto atual da tela
-    ScreenSize().init(context);
+    ScreenSizeLogin().init(context);
+    // Definindo uma largura padrão para botões e inputs (90% da tela)
+    double defaultWidth = ScreenSizeLogin.screenWidth * 0.92;
+
     return Scaffold(
       backgroundColor: const Color(0xFF2E2E2E),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.only(bottom: 75),
-              child: Text(
-                "Instagram", //
-                style: TextStyle(fontSize: 47, color: AppColorsLogin.nameInstagramLogin),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8),
-            child: TextField(
-              maxLength: 30, // Limita a 30 caracteres
-              maxLines: 1, // Garante que o nome de usuário não pule linha
-              decoration: InputDecoration(
-                contentPadding: EdgeInsets.symmetric(vertical: 19),
-                hintText: "Nome de usúario, email ou celular",
-                hintStyle: TextStyle(
-                  color: AppColorsLogin.genericWhite,
-                  fontSize: 15,
-                  wordSpacing: 1,
-                  letterSpacing: 1.5,
-                ),
-                filled: true,
-                fillColor: AppColorsLogin.loginBackground,
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(14),
-                  borderSide: BorderSide(color: AppColorsLogin.borderTextField),
-                ),
-              ),
-            ),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8),
-            child: TextField(
-              maxLength: 30, // Limita a 30 caracteres
-              maxLines: 1, // Garante que o nome de usuário não pule linha
-              decoration: InputDecoration(
-                contentPadding: EdgeInsets.symmetric(vertical: 19),
-                hintText: "Senha",
-                hintStyle: TextStyle(
-                  color: AppColorsLogin.genericWhite,
-                  fontSize: 15,
-                  letterSpacing: 2,
-                ),
-                filled: true,
-                fillColor: AppColorsLogin.loginBackground,
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(14),
-                  borderSide: BorderSide(color: AppColorsLogin.borderTextField),
-                ),
-              ),
-            ),
-          ),
-
-          ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-              backgroundColor: AppColorsLogin.btnEnter,
-              minimumSize: Size(395, 48),
-            ),
-            child: Text(
-              "Entrar",
-              style: TextStyle(
-                color: AppColorsLogin.genericWhite,
-                fontSize: 16.6,
-                // fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-
-          const SizedBox(height: 7),
-
-          TextButton(
-            onPressed: () {},
-            child: Text(
-              "Esqueceu a senha?",
-              style: TextStyle(
-                color: AppColorsLogin.genericWhite,
-                fontSize: 16,
-                letterSpacing: 0.7,
-              ),
-            ),
-          ),
-
-          const SizedBox(height: 12),
-
-          Container(
-            alignment: Alignment.center,
+      body: SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(minHeight: ScreenSizeLogin.screenHeight),
+          child: IntrinsicHeight(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 75),
+                    child: Text(
+                      "Instagram", //
+                      style: TextStyle(fontSize: 47, color: AppColorsLogin.nameInstagramLogin),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: ScreenSizeLogin.screenWidth * 0.02),
+                  child: TextField(
+                    maxLength: 30, // Limita a 30 caracteres
+                    maxLines: 1, // Garante que o nome de usuário não pule linha
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(
+                        vertical: ScreenSizeLogin.screenHeight * 0.024,
+                        horizontal: ScreenSizeLogin.screenWidth * 0.03,
+                      ),
+                      hintText: "Nome de usúario, email ou celular",
+                      hintStyle: TextStyle(
+                        color: AppColorsLogin.genericWhite,
+                        fontSize: 15,
+                        wordSpacing: 1,
+                        letterSpacing: 1.5,
+                      ),
+                      filled: true,
+                      fillColor: AppColorsLogin.loginBackground,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(14),
+                        borderSide: BorderSide(color: AppColorsLogin.borderTextField),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(14),
+                        borderSide: BorderSide(color: AppColorsLogin.borderTextfield),
+                      ),
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: ScreenSizeLogin.screenWidth * 0.02),
+                  child: TextField(
+                    maxLength: 30, // Limita a 30 caracteres
+                    maxLines: 1, // Garante que o nome de usuário não pule linha
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(
+                        vertical: ScreenSizeLogin.screenHeight * 0.024,
+                        horizontal: ScreenSizeLogin.screenWidth * 0.03,
+                      ),
+                      hintText: "Senha",
+                      hintStyle: TextStyle(
+                        color: AppColorsLogin.genericWhite,
+                        fontSize: 15,
+                        letterSpacing: 2,
+                      ),
+                      filled: true,
+                      fillColor: AppColorsLogin.loginBackground,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(14),
+                        borderSide: BorderSide(color: AppColorsLogin.borderTextField),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(14),
+                        borderSide: BorderSide(color: AppColorsLogin.borderTextfield, width: 1.5),
+                      ),
+                    ),
+                  ),
+                ),
+
                 ElevatedButton(
                   onPressed: () {},
-
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: AppColorsLogin.loginBackground,
-                    minimumSize: Size(395, 48),
-                    side: const BorderSide(color: AppColorsLogin.btnEnter),
+                    backgroundColor: AppColorsLogin.btnEnter,
+                    minimumSize: Size(defaultWidth * 1.03, 48),
                   ),
                   child: Text(
-                    "Criar nova conta",
+                    "Entrar",
+                    style: TextStyle(color: AppColorsLogin.genericWhite, fontSize: 16.6),
+                  ),
+                ),
+
+                const SizedBox(height: 7),
+
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Esqueceu a senha?",
                     style: TextStyle(
-                      color: AppColorsLogin.btnEnter,
-                      fontSize: 16.6,
-                      fontWeight: FontWeight.bold,
+                      color: AppColorsLogin.genericWhite,
+                      fontSize: 16,
                       letterSpacing: 0.7,
                     ),
                   ),
                 ),
 
-                const SizedBox(height: 15),
+                const SizedBox(height: 12),
 
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    FaIcon(
-                      FontAwesomeIcons.meta, // O 's' no final de Icons é fundamental
-                      color: AppColorsLogin.genericWhite,
-                      size: 17,
-                    ),
-                    const SizedBox(width: 7),
-                    Text(
-                      "Meta",
-                      style: TextStyle(
-                        color: AppColorsLogin.genericWhite,
-                        fontSize: 17,
-                        fontWeight: FontWeight.bold,
+                Container(
+                  alignment: Alignment.center,
+                  child: Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {},
+
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: AppColorsLogin.loginBackground,
+                          minimumSize: Size(defaultWidth * 1.03, 48),
+                          side: const BorderSide(color: AppColorsLogin.btnEnter),
+                        ),
+                        child: Text(
+                          "Criar nova conta",
+                          style: TextStyle(
+                            color: AppColorsLogin.btnEnter,
+                            fontSize: 16.6,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 0.7,
+                          ),
+                        ),
                       ),
-                    ),
-                  ],
+
+                      const SizedBox(height: 15),
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          FaIcon(
+                            FontAwesomeIcons.meta, // O 's' no final de Icons é fundamental
+                            color: AppColorsLogin.genericWhite,
+                            size: 17,
+                          ),
+                          const SizedBox(width: 7),
+                          Text(
+                            "Meta",
+                            style: TextStyle(
+                              color: AppColorsLogin.genericWhite,
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
