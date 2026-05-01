e7c781ce5a2d21ff57db175c258277046191dd4d
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-05-01 11:01:06 -0300

    nova cor adicionado

--- themeColors/appColors.dart
+++ themeColors/appColors.dart
@@ -5,5 +5,6 @@ class AppColorsLogin {
   static const Color nameInstagramLogin = Color(0xFFFFFEFE);
   static const Color borderTextField = Color(0xFF8B8B8B);
   static const Color genericWhite = Color(0xFFFFFFFF);
-  static const Color btnEnter = Color.fromARGB(255, 26, 112, 182);
+  static const Color btnEnter = Color(0xFF1A70B6);
+  static const Color borderTextfield = Color(0xFFB3B3B3);
 }
