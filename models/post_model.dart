c4e63eca16b6f32aa60f6618562cf129477637c4
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-05-05 22:20:30 -0300

    feat: model criado

--- models/post_model.dart
+++ models/post_model.dart
@@ -1,15 +1,22 @@
-import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
+// 1. O MODELO (O molde do seu post)
 class PostModel {
-  final String userImage;
-  final String userName;
-  final String postImage;
-  final String caption;
+  final int userId; // Na API original é int
+  final int id; // Na API original é int
+  final String title;
+  final String body;
 
-  PostModel({
-    required this.userImage,
-    required this.userName,
-    required this.postImage,
-    required this.caption,
-  });
+  PostModel({required this.userId, required this.id, required this.title, required this.body});
+
+  // Fábrica que transforma o JSON da internet em um objeto Dart
+  factory PostModel.fromJson(Map<String, dynamic> json) {
+    return PostModel(
+      userId: json["userId"] ?? '',
+      id: json["id"] ?? '',
+      title: json["title"] ?? '',
+      body: json["body"] ?? '',
+    );
+  }
 }
