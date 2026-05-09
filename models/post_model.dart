8971998d9cdc174570fcffdec7f4e5000e0bcb2b
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-05-09 11:23:06 -0300

    comentarios nas utilidades + imageUrl: null para provider preencher depois

--- models/post_model.dart
+++ models/post_model.dart
@@ -1,6 +1,3 @@
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-
 // 1. O MODELO (O molde do seu post)
 class PostModel {
   final int userId; // Na API original é int
@@ -22,12 +19,12 @@ class PostModel {
   // Fábrica que transforma o JSON da internet em um objeto Dart
   factory PostModel.fromJson(Map<String, dynamic> json) {
     return PostModel(
-      userId: json["userId"] ?? '',
-      id: json["id"] ?? '',
-      imageUrl: json["url"] ?? '',
-      title: json["title"] ?? '',
-      body: json["body"] ?? '',
-      username: json["username"],
+      userId: json["userId"] ?? 0, // Se for nulo, vira o número 0
+      id: json["id"] ?? 0, // Se for nulo, vira o número 0
+      imageUrl: null, // Começa nulo e o Provider preenche depois
+      title: json["title"] ?? '', // Texto vazio se for nulo
+      body: json["body"] ?? '', // Texto vazio se for nulo
+      username: null, // Começa nulo e o Provider preenche depois
     );
   }
 }
