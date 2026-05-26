a152e898299c8f1e68166dc18e86e5c501f75aad
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-05-25 21:45:55 -0300

    Inicinado o firebase pela main.dart

--- main.dart
+++ main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart'; // 🌟 Importa o núcleo do Firebase
+import 'firebase_options.dart'; // 🌟 O arquivo secreto que o CLI gerou para você!
 import 'package:instagram_app/providers/model_provider.dart';
 import 'package:instagram_app/providers/post_provider.dart';
 import 'package:instagram_app/screens/homeScreen.dart';
@@ -6,7 +8,14 @@ import 'package:instagram_app/screens/loginScreen.dart';
 import 'package:instagram_app/screens/postDetailScreen.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  // 🧠 CONCEITO APRENDIZADO: WidgetsFlutterBinding
+  // Esse comando avisa a engine do Flutter (C++) para preparar os canais de comunicação
+  // com o sistema operacional (Android/iOS) antes de rodar qualquer código assíncrono.
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Inicializa o Firebase passando as configurações geradas pelo CLI de forma segura
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     MultiProvider(
       providers: [
