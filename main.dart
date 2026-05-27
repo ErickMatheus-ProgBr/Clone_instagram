abdf390d3fd6a66d0ed262477de75c5564067a23
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-05-26 22:28:35 -0300

    feat: nova rota nomeada no main.dart

--- main.dart
+++ main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart'; // 🌟 Importa o núcleo do Firebase
+import 'package:instagram_app/screens/LoginFolder/resetPassword.dart';
 import 'firebase_options.dart'; // 🌟 O arquivo secreto que o CLI gerou para você!
 import 'package:instagram_app/providers/model_provider.dart';
 import 'package:instagram_app/providers/post_provider.dart';
 import 'package:instagram_app/screens/homeScreen.dart';
-import 'package:instagram_app/screens/loginScreen.dart';
+import 'package:instagram_app/screens/LoginFolder/homeLogin.dart';
 import 'package:instagram_app/screens/postDetailScreen.dart';
 import 'package:provider/provider.dart';
 
@@ -40,6 +41,7 @@ class MyApp extends StatelessWidget {
         "/": (context) => const Loginscreen(),
         "/homeScreen": (context) => const Homescreen(),
         "/postDetail": (context) => const Postdetailscreen(),
+        "/resetPassword": (context) => const ResetPassword(),
       },
     );
   }
