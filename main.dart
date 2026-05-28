59627cf9acd222b3c0b19c7880ab9bbae03195fa
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-05-27 21:14:05 -0300

    feat: nova rota noemada (tela de verificar email)

--- main.dart
+++ main.dart
@@ -42,6 +42,7 @@ class MyApp extends StatelessWidget {
         "/homeScreen": (context) => const Homescreen(),
         "/postDetail": (context) => const Postdetailscreen(),
         "/resetPassword": (context) => const ResetPassword(),
+        "/veryfyEmail": (context) => const ResetPassword(),
       },
     );
   }
