2b43e0de5905808ee12c73f6d3ebca5292104dd1
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-06-02 20:13:51 -0300

    fix/style/refactor: backbutton no appBar, background linear gradient no body

--- screens/LoginFolder/4.chargePassword.dart
+++ screens/LoginFolder/4.chargePassword.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Chargepassword extends StatefulWidget {
   const Chargepassword({super.key});
@@ -10,6 +11,26 @@ class Chargepassword extends StatefulWidget {
 class _ChargepasswordState extends State<Chargepassword> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.white);
+    return Scaffold(
+      appBar: AppBar(title: Text("Redefinir sua Senha"), centerTitle: true, leading: BackButton()),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomRight,
+            end: Alignment.centerLeft,
+
+            colors: [
+              Color.fromARGB(255, 121, 76, 151),
+              Color.fromARGB(255, 161, 76, 76),
+              Color.fromARGB(255, 29, 29, 28),
+            ],
+          ),
+        ),
+        child: Container(decoration: BoxDecoration(color: Colors.black.withOpacity(0.65))),
+      ),
+    );
   }
 }
