53a9006f187fe88aa29464bf5a6139f292a0da7e
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-06-02 19:30:39 -0300

    feat: tela de redefinição de senha

---
+++ screens/LoginFolder/chargePassword.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Chargepassword extends StatefulWidget {
+  const Chargepassword({super.key});
+
+  @override
+  State<Chargepassword> createState() => _ChargepasswordState();
+}
+
+class _ChargepasswordState extends State<Chargepassword> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
