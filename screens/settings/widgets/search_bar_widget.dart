7579d6c7d78687e2c5c700a204dc5cc5845bc8c8
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-06-10 20:11:53 -0300

    feat: novo campo de buscar (search), opção na tela de configurações

--- screens/settings/widgets/search_bar_widget.dart
+++ screens/settings/widgets/search_bar_widget.dart
@@ -5,6 +5,25 @@ class MySearchBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.white);
+    return Container(
+      height: 40,
+      margin: EdgeInsets.symmetric(horizontal: 11, vertical: 18),
+      child: TextField(
+        style: TextStyle(fontSize: 18.4, letterSpacing: 0.3, fontWeight: FontWeight.bold),
+        decoration: InputDecoration(
+          hintText: "Pesquisar",
+          hintStyle: TextStyle(color: const Color(0xFF6B6767)),
+          prefixIcon: Padding(
+            padding: const EdgeInsets.only(left: 10, right: 5, bottom: 1),
+            child: Icon(Icons.search_outlined, color: Color(0xFF6B6767)),
+          ),
+          prefixIconConstraints: BoxConstraints(minHeight: 2, maxWidth: 40),
+          filled: true,
+          fillColor: const Color.fromARGB(255, 39, 37, 37),
+          contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+          enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+        ),
+      ),
+    );
   }
 }
