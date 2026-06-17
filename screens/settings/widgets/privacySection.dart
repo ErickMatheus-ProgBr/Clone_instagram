8bf7dc81e05cff7414223e88f3d1b4d8a75328a9
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-06-17 18:37:03 -0300

    feat(settings): refatora telas de configurações para mapeamento dinâmico

--- screens/settings/widgets/privacySection.dart
+++ screens/settings/widgets/privacySection.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_app/screens/settings/widgets/itens_settings/settings_tile_widget.dart';
+import 'package:instagram_app/screens/settings/widgets/itens_settings/usage_data.dart';
 
 class PrivacySection extends StatelessWidget {
   const PrivacySection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final items = UsageData.getUsageItems(context);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          "Quem pode ver seus conteúdo",
+          style: TextStyle(fontSize: 14.5, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        const SizedBox(height: 8),
+
+        ...items.map((dynamicItem) => SettingsTile(item: dynamicItem)).toList(),
+      ],
+    );
   }
 }
