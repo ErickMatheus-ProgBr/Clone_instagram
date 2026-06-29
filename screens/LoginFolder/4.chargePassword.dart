6a3a66001e9531242685c491e23702c81c6b4f33
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-06-29 20:20:43 -0300

    feat(auth): implementa tela chargePassword para redefinição de senha no fluxo de login

--- screens/LoginFolder/4.chargePassword.dart
+++ screens/LoginFolder/4.chargePassword.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:instagram_app/core/utils/media_query.dart';
 
 class Chargepassword extends StatefulWidget {
