c2bd38bf1edb67c54de59ba337c8a56fa2157dc4
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-06-29 20:18:13 -0300

    fix(settings): ajusta alinhamento do botao Sair e organiza seções da tela

--- screens/settings/settings_screen.dart
+++ screens/settings/settings_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:instagram_app/screens/settings/widgets/privacySection.dart';
-import 'package:instagram_app/screens/settings/widgets/search_bar_widget.dart';
-import 'package:instagram_app/screens/settings/widgets/accountSection.dart';
-import 'package:instagram_app/screens/settings/widgets/usageSection.dart';
+import 'package:instagram_app/screens/settings/widgets/drawer/privacySection.dart';
+import 'package:instagram_app/screens/settings/widgets/drawer/search_bar_widget.dart';
+import 'package:instagram_app/screens/settings/widgets/drawer/accountSection.dart';
+import 'package:instagram_app/screens/settings/widgets/drawer/usageSection.dart';
 
 class DrawerCustom extends StatelessWidget {
   const DrawerCustom({super.key});
@@ -40,6 +40,22 @@ class DrawerCustom extends StatelessWidget {
             UsageSection(), // Widget do bloco "Como usar o Instagram"
             Divider(thickness: 2, color: Color(0xFF262626)),
             PrivacySection(), // Widget do bloco "Quem pode ver"
+            Divider(thickness: 2, color: Color(0xFF262626)),
+
+            Align(
+              alignment: Alignment.centerLeft, // 🟢 Força tudo que está dentro a ir para a esquerda
+              child: TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets
+                      .zero, // Remove os espaçamentos internos padrões do botão para colar bem na esquerda
+                ),
+                child: const Text(
+                  "Sair",
+                  style: TextStyle(color: Color(0xFFFFAD4E47), fontSize: 19, letterSpacing: 1),
+                ),
+              ),
+            ),
           ],
         ),
       ),
