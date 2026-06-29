805443e03e07ffe712203ce96c37fbcb1f96666a
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-06-29 20:12:09 -0300

    feat(main): inicializacao do firebase, setup da camera e novos providers

--- main.dart
+++ main.dart
@@ -7,18 +7,22 @@ import 'package:instagram_app/screens/LoginFolder/auth_check.dart';
 import 'firebase_options.dart'; // 🌟 O arquivo secreto que o CLI gerou para você!
 import 'package:instagram_app/providers/model_provider.dart';
 import 'package:instagram_app/providers/post_provider.dart';
-import 'package:instagram_app/screens/homeScreen.dart';
-import 'package:instagram_app/screens/LoginFolder/1.homeLogin.dart';
-import 'package:instagram_app/screens/postDetailScreen.dart';
+import 'package:instagram_app/screens/Home/homeScreen.dart';
+import 'package:instagram_app/screens/Feed/postDetailScreen.dart';
 import 'package:provider/provider.dart';
-import 'auth_check.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:camera/camera.dart';
+
+late CameraDescription firstCamera;
 
 void main() async {
   // 🧠 CONCEITO APRENDIZADO: WidgetsFlutterBinding
   // Esse comando avisa a engine do Flutter (C++) para preparar os canais de comunicação
   // com o sistema operacional (Android/iOS) antes de rodar qualquer código assíncrono.
   WidgetsFlutterBinding.ensureInitialized();
+
+  final cameras = await availableCameras();
+
+  firstCamera = cameras.first;
 
   // Inicializa o Firebase passando as configurações geradas pelo CLI de forma segura
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
@@ -43,8 +47,13 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
-        "/": (context) => const AuthCheck(),
-        "/homeScreen": (context) => const Homescreen(),
+        // 🟢 1. Passamos a câmera global para o verificador de autenticação (sem const)
+        "/": (context) => AuthCheck(camera: firstCamera),
+
+        // 🟢 2. Corrigido de 'firstca' para 'firstCamera' e removido o 'const'
+        "/homeScreen": (context) => Homescreen(camera: firstCamera),
+
+        // As outras rotas não usam a câmera, então podem continuar com const!
         "/resetPassword": (context) => const ResetPassword(),
         "/postDetail": (context) => const Postdetailscreen(),
         "/verifyEmail": (context) => const VerifyEmail(),
